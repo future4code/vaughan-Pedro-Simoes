56285a01aa814db9937430f695211e2ca66d4364
pedro simoes <pmourasimoes@gamil.com>
2021-12-01 13:29:43 -0300

    strings-arrays-adicionado

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,17 @@
+const nome = prompt ("Qual e o seu nome")
+const egmail = prompt ("Qual e o seu eGmail")
+console.log ("O eGmail é ", + egmail, + "seja bem vindoo ", + nome )
+const listaDeComida = ["Macarrao, hamburgue, lazanha, Churrasco, Bife acebolado"]
+console.log(listaDeComida)
+console.log("Estas comidas sao as minhas preferidas" + listaDeComida )
+const comidaUsoario = prompt ("digite uma comidas preferidas")
+const comidaUsoario2 = prompt ("digite outra comidas preferidas")
+console.log (comidaUsoario2 + comidaUsoario)
+const listaDeTarefas = []
+const pergunta = prompt ("Digite 3 tarefas que voce tenha que fazer no dia")
+const listaDeTarefas2 = ["Acordar, tomar cafe, dar comida para os aminais" ]
+console.log(listaDeTarefas2)
+const pergunta4 = prompt ("o índice de uma tarefa que ele já realizou: 0, 1 ou 2 ")
+listaDeTarefas2.splice(0,1,2)
+console.log(listaDeTarefas2)
+
