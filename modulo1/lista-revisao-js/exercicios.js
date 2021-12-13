f60a0d1014b21025d1929611f1af5949d1a66ec3
pedro simoes <pmourasimoes@gamil.com>
2021-12-13 13:38:30 -0300

    add-revisao

---
+++ modulo1/lista-revisao-js/exercicios.js
@@ -0,0 +1,111 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+   const novaArray = [1, 2, 4, 6, 9]
+   console.log(novaArray.length)
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+    const novaArray = [1, 2, 4, 6, 9]
+    novaArray.reverse()
+    console.log (novaArray)
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+    const ordenadoArray = [13, 21, 12, 7, 9]
+    ordenadoArray.sort
+    console.log (ordenadoArray)
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+    const numero = [1, 10, 23, 20, 21, 12 ]
+    if(numero % 2 === 0) {
+        console.log("O número é par");
+    }
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    const numero = [1, 10, 23, 20, 21, 12 ]
+    if(numero % 2 === 0) {
+        const resultado = Math.numero()
+    }
+    }
+
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+    const numero = [1, 10, 23, 20, 21, 12 ]
+    console.log(Math.max(numero))
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+const numeros = [10, 20]
+    maiorNumero = 20
+maiorDivisivelPorMenor = true
+diferenca = 10
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   const numerosPares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+   if(numero % 2 === 0) {
+console.log("este sao os numeros pares")
+   }
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+if(ladosTriangulo) {
+const Escaleno = (lado1 !== lado2 !== lado3) 
+const Isósceles = (lado4 === lado5 !== lado6)
+const Equilátero = (lado7 === lado8 === lado9)
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
